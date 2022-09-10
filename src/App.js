40c07a8c20d385e1f50bd8ac21928439c582a91e
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-09-10 11:21:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
 
 function App() {
+  const [musicData, setMusicData] = useState(data);
+
+  const filter = (category) => {
+    if (category === "all") setMusicData(data);
+    else {
+      setMusicData(data.filter((item) => item.category === category));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="main">
+      <header>Music Recommendation App</header>
+      <div className=" outer-container">
+        <div className=" buttonGrid flex justify-center p-4 gap-9">
+          <button onClick={() => filter("all")}>All</button>
+          <button onClick={() => filter("pop")}>Pop</button>
+          <button onClick={() => filter("indianClassic")}>
+            Indian Classic
+          </button>
+          <button onClick={() => filter("jazz")}>Jazz</button>
+        </div>
+        <div className=" container grid grid-cols-4 gap-5 gap-y-5">
+          {musicData.map((item) => (
+            <div
+              key={item.id}
+              className=" card bg-white p-2 flex flex-col justify-center items-center rounded-lg"
+            >
+              <img
+                src={item.image}
+                className=" w-[15rem] h-[15rem] rounded-lg"
+                alt=""
+              />
+              <span>{item.rating}⭐</span>
+              <h1>{item.title}</h1>
+              <p className=" underline">{item.artist}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="social-icons ">
+        <p className="footer">Copyright &copy; 2022 - Gurudatt Puranik</p>
+        <a href="https://twitter.com/PuranikGurudatt">
+          <i className="fa-brands fa-twitter"></i>
         </a>
-      </header>
+        <a href="https://www.linkedin.com/in/gurudatt-puranik-0933b0195/">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://www.instagram.com/gurudatt_puranik/">
+          <i className="fa-brands fa-square-instagram"></i>
+        </a>
+        <a href="https://github.com/gurudattpuranik25">
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </div>
+      <p className="responsiveFooter">
+        Copyright &copy; 2022 - Gurudatt Puranik
+      </p>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,138 @@
+import smellsLikeMe from "./images/smellsLikeMe.JPG";
+import imGood from "./images/imGood.JPG";
+import attention from "./images/attention.JPG";
+import holdMeCloser from "./images/holdMeCloser.JPG";
+import dream from "./images/dream.JPG";
+import ragaKirwani from "./images/ragaKirwani.JPG";
+import abMoriBaat from "./images/abMoriBaat.JPG";
+import raagaBhairavi from "./images/raagaBhairavi.JPG";
+import circle from "./images/circle.JPG";
+import sunshine from "./images/sunshine.JPG";
+import comeAwayWithMe from "./images/comeAwayWithMe.JPG";
+import theWayLoveGoes from "./images/theWayLoveGoes.JPG";
+import summerHigh from "./images/summerHigh.JPG";
+import kaliyuga from "./images/kaliyuga.JPG";
+import morning from "./images/morning.JPG";
+
+export const data = [
+  {
+    id: 1,
+    title: "Smells Like Me",
+    artist: "Charlie Puth",
+    image: smellsLikeMe,
+    rating: 4.2,
+    category: "pop",
+  },
+  {
+    id: 2,
+    title: "I'm Good (Blue)",
+    artist: "David Guetta",
+    image: attention,
+    rating: 3.8,
+    category: "pop",
+  },
+  {
+    id: 3,
+    title: "Attention",
+    artist: "New Jeans",
+    image: imGood,
+    rating: 5,
+    category: "pop",
+  },
+  {
+    id: 4,
+    title: "Hold Me Closer",
+    artist: "Elton John",
+    image: holdMeCloser,
+    rating: 4.5,
+    category: "pop",
+  },
+  {
+    id: 5,
+    title: "Dream (Raag Tilak Nat)",
+    artist: "Niladri Kumar",
+    image: dream,
+    rating: 4,
+    category: "indianClassic",
+  },
+  {
+    id: 6,
+    title: "Raga Kirwani-Alap Gat-Teental",
+    artist: "Iqbal Hussain",
+    image: ragaKirwani,
+    rating: 3.9,
+    category: "indianClassic",
+  },
+  {
+    id: 7,
+    title: "Ab Mori Baat",
+    artist: "Anirudh Varma",
+    image: abMoriBaat,
+    rating: 4.7,
+    category: "indianClassic",
+  },
+  {
+    id: 8,
+    title: "Raga Bhairavi",
+    artist: "Sangit Om",
+    image: raagaBhairavi,
+    rating: 4.1,
+    category: "indianClassic",
+  },
+  {
+    id: 9,
+    title: "The Circle",
+    artist: "Tord Gustavsen Trio",
+    image: circle,
+    rating: 4.9,
+    category: "jazz",
+  },
+  {
+    id: 10,
+    title: "Ain't No Sunshine",
+    artist: "Nara",
+    image: sunshine,
+    rating: 3.4,
+    category: "jazz",
+  },
+  {
+    id: 11,
+    title: "Come Away With Me",
+    artist: "Norah Jones",
+    image: comeAwayWithMe,
+    rating: 3.8,
+    category: "jazz",
+  },
+  {
+    id: 12,
+    title: "That's The Way Love Goes",
+    artist: "Norman Brown",
+    image: theWayLoveGoes,
+    rating: 4.4,
+    category: "jazz",
+  },
+  {
+    id: 13,
+    title: "Summer High",
+    artist: "AP Dhillon",
+    image: summerHigh,
+    rating: 4.8,
+    category: "pop",
+  },
+  {
+    id: 14,
+    title: "kaliyugavaradana",
+    artist: "U Srinivas",
+    image: kaliyuga,
+    rating: 4.8,
+    category: "indianClassic",
+  },
+  {
+    id: 15,
+    title: "Small Hours Of The Morning",
+    artist: "Gerry Mulligan Sextet",
+    image: morning,
+    rating: 3.1,
+    category: "jazz",
+  },
+];
